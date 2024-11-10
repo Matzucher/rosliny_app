ab7769d79b150eb2d980b31e9c44a3bab92f6c41
Mateusz_Zuchowski <153012588+Matzucher@users.noreply.github.com>
2024-11-10 21:32:16 +0100

    kalendarz update

--- app/(tabs)/kalendarz.tsx
+++ app/(tabs)/kalendarz.tsx
@@ -1,15 +1,72 @@
-import { Text, View } from "react-native";
+import { CalendarList, LocaleConfig } from 'react-native-calendars';
+import React, { useState } from 'react';
+import { Calendar } from 'react-native-calendars';
+LocaleConfig.locales['pl'] = {
+    monthNames: [
+        'Styczeń',
+        'Luty',
+        'Marzec',
+        'Kwiecień',
+        'Maj',
+        'Czerwiec',
+        'Lipiec',
+        'Sierpień',
+        'Wrzesień',
+        'Październik',
+        'Listopad',
+        'Grudzień'
+    ],
+    monthNamesShort: ['Sty', 'Lut', 'Mar', 'Kwi', 'Maj', 'Cze', 'Lip', 'Sie', 'Wrz', 'Paź', 'Lis', 'Gru'],
+    dayNames: ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'],
+    dayNamesShort: ['Pon', 'Wt', 'Śr', 'Czw', 'Pt', 'Sob', 'Nie'],
+    today: "Dziś: "
+};
 
-export default function Kalendarz() {
+LocaleConfig.defaultLocale = 'pl';
+
+const App = () => {
+    const [selected, setSelected] = useState('');
+
+
     return (
-        <View
+        <CalendarList
+            onVisibleMonthsChange={(months) => { console.log('now these months are visible', months); }}
+            pastScrollRange={50}
+            futureScrollRange={50}
+            scrollEnabled={true}
+            showScrollIndicator={false}
+            showWeekNumbers={true}
+            hideExtraDays={false}
             style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
+                borderWidth: 1,
+                borderColor: 'gray',
+                height: 350,
             }}
-        >
-            <Text>Edit app/index.tsx to edit this screen.</Text>
-        </View>
+            theme={{
+                backgroundColor: '#c2fcf9',
+                calendarBackground: '#5f5f5f',
+                textSectionTitleColor: '#c2fcf9',
+                selectedDayBackgroundColor: '#c2fcf9',
+                selectedDayTextColor: '#000000',
+                todayTextColor: '#00adf5',
+                dayTextColor: '#44ff00',
+                textDisabledColor: '#b4e0bf',
+                monthTextColor: 'white',
+                textDayFontSize: 20,
+                textMonthFontSize: 20,
+                textDayHeaderFontSize: 16
+
+            }}
+            onDayPress={day => {
+                setSelected(day.dateString);
+            }}
+            markedDates={{
+                [selected]: { selected: true, disableTouchEvent: true },
+            }}
+        />
+
     );
-}
+
+};
+
+export default App;
