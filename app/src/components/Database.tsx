b150060ef51d58648185ceb0a2519a885f140e28
Sikora_Jakub <153021432+Rogat3q@users.noreply.github.com>
2024-11-15 18:12:06 +0100

    Update Database.tsx

--- app/src/components/Database.tsx
+++ app/src/components/Database.tsx
@@ -9,6 +9,23 @@ const rosliny: roslina[] = [
     { id: 1, nazwa: "Aglaonema", okres_podlewania_zima: 12, okres_podlewania_latem: 10, },
     { id: 2, nazwa: "Albuca Spiralis", okres_podlewania_zima: 20, okres_podlewania_latem: 20 },
     { id: 3, nazwa: "Aloes", okres_podlewania_zima: 20, okres_podlewania_latem: 20 },
-];
+    { id: 4, nazwa: "Alokazja", okres_podlewania_zima: 10, okres_podlewania_latem: 12 },
+    { id: 5, nazwa: "Alternanthera ficoidea", okres_podlewania_zima: 10, okres_podlewania_latem: 12 },
+    { id: 6, nazwa: "Ananas champaca", okres_podlewania_zima: 10, okres_podlewania_latem: 12 },
+    { id: 7, nazwa: "Ananas mi amigo", okres_podlewania_zima: 10, okres_podlewania_latem: 12 },
+    { id: 8, nazwa: "Anthurium", okres_podlewania_zima: 10, okres_podlewania_latem: 12 },
+    { id: 9, nazwa: "Areka palma", okres_podlewania_zima: 10, okres_podlewania_latem: 12 },
+    { id: 10, nazwa: "Aspidistra wyniosła", okres_podlewania_zima: 10, okres_podlewania_latem: 12 },
+    { id: 11, nazwa: "Bananowiec", okres_podlewania_zima: 3, okres_podlewania_latem: 4 },
+    { id: 12, nazwa: "Begonia", okres_podlewania_zima: 10, okres_podlewania_latem: 12 },
+    { id: 13, nazwa: "Begonia maculata tamaya", okres_podlewania_zima: 10, okres_podlewania_latem: 12 },
+    { id: 14, nazwa: "Caladium", okres_podlewania_zima: 10, okres_podlewania_latem: 12 },
+    { id: 15, nazwa: "Calathea", okres_podlewania_zima: 5, okres_podlewania_latem: 7 },
+    { id: 16, nazwa: "Cereus", okres_podlewania_zima: 10, okres_podlewania_latem: 12 },
+    { id: 17, nazwa: "Ceropegia", okres_podlewania_zima: 10, okres_podlewania_latem: 12 },
+    { id: 18, nazwa: "Chamedora", okres_podlewania_zima: 10, okres_podlewania_latem: 12 },
+    { id: 19, nazwa: "Cissus", okres_podlewania_zima: 10, okres_podlewania_latem: 12 },
+    { id: 20, nazwa: "Clusia rosea", okres_podlewania_zima: 10, okres_podlewania_latem: 12 },
+    ];
 
 export default rosliny;
