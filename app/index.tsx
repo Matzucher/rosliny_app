d95d3cda26e34d9725461a3ce5d69082bfcd0299
JK2233 <93614447+JK2233@users.noreply.github.com>
2024-11-15 17:26:55 +0100

    testing

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { PlantsContext, PlantsProvider } from './src/components/RoslinyContext';
 
+// if ('serviceWorker' in navigator) {
+// 	// Register a service worker hosted at the root of the
+// 	// site using the default scope.
+// 	navigator.serviceWorker.register(`./app/service-worker.js`).then(
+// 	  registration => {
+// 		 console.log('Service worker registration succeeded:', registration);
+// 	  },
+// 	  /*catch*/ error => {
+// 		 console.error(`Service worker registration failed: ${error}`);
+// 	  }
+// 	);
+// } else {
+// 	console.error('Service workers are not supported.');
+// }
+
 const App: React.FC = () => {
     return (
         <PlantsProvider>
